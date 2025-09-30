0bc829631936bb213d2ecbd575042655df9bda27
EnricoSpinardi <167817620+EnricoSpinardi@users.noreply.github.com>
2025-09-30 14:54:30 -0300

    NodeJS - CREATE cadastro em Realtime Database

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDbohDC8RRSL0kY7K2vuGZLawknR2MMRXo",
   authDomain: "nodejs-cf0bf.firebaseapp.com",
@@ -13,10 +9,11 @@ const firebaseConfig = {
   projectId: "nodejs-cf0bf",
   storageBucket: "nodejs-cf0bf.firebasestorage.app",
   messagingSenderId: "951622214945",
-  appId: "1:951622214945:web:88b0237f5591a753074a60",
-  measurementId: "G-5MKEDJJXY9"
+  appId: "1:951622214945:web:470027144e271b02074a60",
+  measurementId: "G-Y4XQ0WPE27"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Evita reinicialização em hot-reload (nodemon)
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const db = getDatabase(app);
